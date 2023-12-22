660fd3afc4b35769f2572df00f275888be4f87a2
Samsu <biosamsuddin@gmail.com>
2023-12-22 11:01:47 +0530

    hi

--- model/ProductSchema.js
+++ model/ProductSchema.js
@@ -20,23 +20,6 @@ const productSchema = new Schema({
     deleted: { type : Boolean, default: false},
 })
 
-/*
-const virtualId  = productSchema.virtual('id');
-virtualId.get(function(){
-    return this._id;
-})
-*/
-// we can't sort using the virtual fields. better to make this field at time of doc creation
-// const virtualDiscountPrice =  productSchema.virtual('discountPrice');
-// virtualDiscountPrice.get(function(){
-//     return Math.round(this.price*(1-this.discountPercentage/100));
-// })
-/*
-productSchema.set('toJSON',{
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc,ret) { delete ret._id}
-})
-*/
+
 
 exports.Product = mongoose.model('Product',productSchema)

7ef438851ec53a040358daacf4ca901103f35878
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-03-31 14:58:14 +0530

    merged

--- router/Order.js
+++ router/Order.js
@@ -3,13 +3,8 @@ const { FetchOrders,CreateOrders } = require('../controller/Order');
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.get('/', CreateOrders)
-.get('/send-orders',FetchOrders)
-=======
 router.get('/', FetchOrders)
 .get('/id:')
->>>>>>> origin/main
       
 
 

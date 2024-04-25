584fa7b786c19ce3ee22668e9be4926d7b6db71d
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-04-25 16:05:01 +0530

    k

---
+++ router/auth.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const {createUser,getUserbyEmail,getAllUser,getUserbyId,generateOTP,verifyOtp}=require('../controller/auth')
+const User = require('../model/UserSchema');
+const router = express.Router();
+
+router.post('/signup',createUser )
+      .post('/login',getUserbyId)
+       .post('/send-otp/',generateOTP)
+       .post('/verify-otp',verifyOtp)
+       .get('/user',getAllUser)
+
+
+
+exports.router = router;
